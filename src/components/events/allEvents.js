110bf6453dc58b756f46dae6ceed002a60be9614
shailus1029 <shailus1029@gmail.com>
2021-08-24 09:52:03 +0530

    event details on all events pop up

--- src/components/events/allEvents.js
+++ src/components/events/allEvents.js
@@ -1,23 +1,68 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Styles from "../../assets/css/event.css";
-import ModalHeader from './modalHeader';
+import ModalHeader from "./modalHeader";
 
 const AllEvents = (props) => {
+    const handleClick = () => {
+        let collapsibleElements = document.getElementsByName("collapsible");
+        let i;
+        for (i = 0; i < collapsibleElements.length; i++) {
+            collapsibleElements[i].addEventListener("click", function (event) {
+                this.classList.toggle("active");
+                let content = this.childNodes[2];
+                if (content.style.display === "block") {
+                    content.style.display = "none";
+                } else {
+                    content.style.display = "block";
+                }
+            });
+        }
+    };
+
+    useEffect(() => {
+        handleClick();
+        return () => {
+            let collapsibleElements = document.getElementsByName("collapsible");
+            let i;
+            for (i = 0; i < collapsibleElements.length; i++) {
+                collapsibleElements[i].removeEventListener("click");
+            }
+        };
+    }, []);
+
     return (
         <div id='allEvents' className={Styles.eventDetailsPopUp}>
             <ModalHeader
                 closeAddEvent={props.closeAddEvent}
-                headerTitle={`Events - ${props.events[0] && new Date(props.events[0].date).toDateString()}`}
+                headerTitle={`Events - ${
+                    props.events[0] &&
+                    new Date(props.events[0].date).toDateString()
+                }`}
             />
             <div className={Styles.allEvents}>
-                {props.events.map((item) => {
+                {props.events.map((item, index) => {
                     return (
-                        <div className={Styles.eventList}>
+                        <button
+                            key={index}
+                            type='button'
+                            name='collapsible'
+                            className={`${Styles.eventList} ${Styles.collapsible}`}
+                        >
                             {item.title}
                             <span>
                                 ({item.startTime} - {item.endTime})
                             </span>
-                        </div>
+                            <div className={Styles.content}>
+                                <div>
+                                    <p>
+                                        <b>Type</b> - {item.type}
+                                    </p>
+                                    <p className={Styles.eventDescriptions}>
+                                        <b>Description</b> - {item.description}
+                                    </p>
+                                </div>
+                            </div>
+                        </button>
                     );
                 })}
             </div>
